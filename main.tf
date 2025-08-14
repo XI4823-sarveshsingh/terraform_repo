cd36426574d157b612dfd95e0f9ae1f5ec6776f5
Sarvesh Singh <sarvesh.singh@xebia.com>
2025-08-14 12:14:20 +0530

    Update vpc-cni addon configuration in main.tf to enable most recent version

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ source  = "terraform-aws-modules/eks/aws"
   cluster_addons = {
     coredns                = {most_recent = true}
     kube-proxy             = {most_recent = true}
-    vpc-cni                = {most_recent = false}
+    vpc-cni                = {most_recent = true}
     external-dns           = {most_recent = true}
     metrics-server         = {most_recent = true}
     eks-pod-identity-agent = {most_recent = true}
